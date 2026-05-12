7ffd769edc0e018360c64634df28ae4f612f8cf5
Maddula Neelaveni <maddualneelaveni12793@example.com>
2026-05-12 22:00:00 +0530

    Palindrome solution added

--- PalindromeNumber.py
+++ PalindromeNumber.py
@@ -3,3 +3,10 @@
 
 # Simple Python solution :
 num=int(input("Enter a number: "))
+if str(num)==str(num)[::-1]:
+    print("Palindrome")
+else:
+    print("Not a palindrome")
+
+# Time complexity: O(n) where n is the number of digits in the number. This is because we need to convert the number to a string and reverse it, which takes O(n) time.
+# Space complexity: O(n) because we are creating a new string to store the reversed version of the number

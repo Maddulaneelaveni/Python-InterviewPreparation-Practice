95de49b3988c39e1e406be55cdb5c6a741ceeaeb
Maddula Neelaveni <maddualneelaveni12793@example.com>
2026-05-12 22:00:00 +0530

    file updated

--- PalindromeNumber.py
+++ PalindromeNumber.py
@@ -10,3 +10,17 @@ else:
 
 # Time complexity: O(n) where n is the number of digits in the number. This is because we need to convert the number to a string and reverse it, which takes O(n) time.
 # Space complexity: O(n) because we are creating a new string to store the reversed version of the number
+
+# DSA solution (without string):
+
+num=int(input("Enter a number: "))
+original_num=num
+reversed_num=0
+while num>0:
+    digit=num%10
+    reversed_num=reversed_num*10+digit
+    num=num//10
+if original_num==reversed_num:
+    print("Palindrome")
+else:
+    print("Not a palindrome")

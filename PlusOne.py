beb9d3859dc09aa39972fc1d9a02233ed3c84b54
Maddula Neelaveni <maddulaneelaveni12793@gmail.com>
2026-05-15 10:00:00 +0530

    Plus One  problem added

--- PlusOne.py
+++ PlusOne.py
@@ -12,3 +12,31 @@ print(list(map(int, str(num))))        # it is used to convert the resulting int
 # Time complexity: 0(n)
 # Space complexity: O(n)
 
+
+# DSA Solution:
+digits = list(map(int, input().split()))      # give simple input like this : 1 2 3
+for i in range(len(digits)-1, -1, -1):        # in simple, we are iterating through the list of digits in reverse order, starting from the last index (len(digits)-1) and ending at the first index (0). The step -1 indicates that we are moving backwards through the list. This allows us to process the digits from right to left, which is necessary when adding one to the number represented by the list of digits.
+    if digits[i] < 9:                     # it is used to check if the current digit is less than 9. If it is, then we can simply increment that digit by 1 and break out of the loop, because we don't need to worry about any carry-over to the next digit. For example, if the current digit is 3, then after this line, it will become 4, and we can stop processing further digits. 
+        digits[i] += 1
+        print(digits)
+        break
+    digits[i] = 0
+else:
+    digits.insert(0, 1)      # it is used to handle the case where all the digits in the list are 9. In this case, when we add one to the number, it will result in a new digit being added at the beginning of the list (the most significant digit). For example, if the input is [9, 9, 9], then after adding one, it will become [1, 0, 0, 0]. To achieve this, we insert a 1 at the beginning of the list using digits.insert(0, 1), and then we set all the existing digits to 0. This is done in the else block because it only executes if we have iterated through all the digits without finding any digit less than 9.
+    print(digits)
+
+# We can also do this in another way by using two pointers, one at the start and one at the end of the list, and we can move the pointers towards each other until we find a digit that is less than 9. Then we can increment that digit and set all the digits to the right of it to 0. If we reach the end of the list without finding a digit less than 9, then we can insert a 1 at the beginning of the list and set all the digits to 0.
+# TWO POINTERS APPROACH:
+
+digits = list(map(int, input().split()))
+left, right = 0, len(digits) - 1
+while right >= 0:
+    if digits[right] < 9:
+        digits[right] += 1
+        print(digits)
+        break
+    digits[right] = 0
+    right -= 1
+else:
+    digits.insert(0, 1)
+    print(digits)

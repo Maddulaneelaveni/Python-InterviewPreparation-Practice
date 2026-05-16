fd89fed1c3365e25ab77de72540e5f1aa2f18d6d
Maddula Neelaveni <maddulaneelaveni12793@gmail.com>
2026-05-16 10:00:00 +0530

    Plus One  problem added

--- AddDigits.py
+++ AddDigits.py
@@ -15,3 +15,6 @@ if num == 0:
     print(0)
 else:
     print(1 + (num - 1) % 9) # in simple, we are calculating the digital root of the number using the formula 1 + (num - 1) % 9. The digital root is the single-digit result obtained by repeatedly adding the digits of a number until only one digit remains. The formula works because the digital root of a number is equivalent to its value modulo 9, with a special case for numbers that are multiples of 9 (where the digital root is 9 instead of 0). By using this formula, we can directly compute the final single-digit result without needing to perform multiple iterations of adding digits together.
+
+# TC: O(1) because we are using a constant amount of time to compute the result using the formula.
+# SC: O(1) because we are using a constant amount of space to store the input number and the result.

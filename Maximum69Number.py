01c481978aeb95ae69441cadb98e5f3b419e516f
Maddula Neelaveni <maddulaneelaveni12793@gmail.com>
2026-05-19 10:00:00 +0530

    Plus One  problem added

--- Maximum69Number.py
+++ Maximum69Number.py
@@ -4,3 +4,6 @@
 # Simple Python solution:
 num = input()
 print(int(num.replace('6', '9', 1)))  # it is used to replace the first occurrence of the digit '6' with '9' in the string representation of the number. The replace() method takes three arguments: the substring to be replaced ('6'), the substring to replace it with ('9'), and the maximum number of occurrences to replace (1). This means that only the first '6' found in the string will be replaced with '9', and any subsequent '6's will remain unchanged. After performing the replacement, we convert the resulting string back to an integer using int() to get the final maximum number.
+
+# TC: O(n) where n is the number of digits in the input number. This is because the replace() method needs to scan through the string to find the first occurrence of '6' and perform the replacement.
+# SC: O(n) because we are creating a new string as a result of the replace operation, which takes up space proportional to the number of digits in the input number.

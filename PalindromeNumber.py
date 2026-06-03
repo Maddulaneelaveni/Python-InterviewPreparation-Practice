c8b1dae14f96cbe80d646f97a3efb15cf3a7508b
Maddula Neelaveni <maddualneelaveni12793@example.com>
2026-06-03 22:00:00 +0530

    file updated

--- PalindromeNumber.py
+++ PalindromeNumber.py
@@ -20,4 +20,7 @@ while num>0:
     digit=num%10
     reversed_num=reversed_num*10+digit
     num=num//10
-
+if original_num==reversed_num:
+    print("Palindrome") 
+else:
+    print("Not a palindrome")

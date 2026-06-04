54a4380da75e8d04ab00a0d5aacf9feeccd8192b
Neelaveni <maddulaneelaveni12793@gmail.com>
2026-06-04 13:52:50 +0530

    Added Python interview practice solutions

---
+++ PalindromeNumber.py
@@ -0,0 +1,5 @@
+# Palindrome number
+# It is a number that reads the same backward as forward. For example, 121 is a palindrome while 123 is not.
+
+# Simple Python solution :
+num=int(input("Enter a number: "))
